4e06628b964c51fa40d24d52255b7f97ebd20397
Valery Li <lee_hobb@rambler.ru>
2023-05-24 20:32:17 +0600

    add friends

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -27,7 +27,7 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  isLoading: boolean;
+  isLoading?: boolean;
 }
 
 const Button: FC<ButtonProps> = ({
